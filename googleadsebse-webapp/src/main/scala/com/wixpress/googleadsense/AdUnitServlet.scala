aa91a696c655c15c4df6423b27ef4d49914f4fed
Daniel Shmuglin <daniels@wix.com>
2012-08-30 21:14:33 +0300

    merges with Tom's Style section

--- googleadsebse-webapp/src/main/scala/com/wixpress/googleadsense/AdUnitServlet.scala
+++ googleadsebse-webapp/src/main/scala/com/wixpress/googleadsense/AdUnitServlet.scala
@@ -3,7 +3,7 @@ package com.wixpress.googleadsense
 import domain.WidgetId
 import org.scalatra.ScalatraServlet
 import org.scalatra.scalate.ScalateSupport
-import Environment._
+import com.wixpress.googleadsense.Environment.application._
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,8 +17,9 @@ class AdUnitServlet extends ScalatraServlet with ScalateSupport {
   }
 
   get("/") {
+    val settings = settingsService.getSettings(WidgetId(params("instance"), params("compId")))
     ssp("adunit.ssp",
-      "contextRoot" -> request.getContextPath)
-
+      "contextRoot" -> request.getContextPath,
+      "settings" -> settings)
   }
 }
