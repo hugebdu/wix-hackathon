f836bc04afa3af1d06783f63d719d2c463fb0220
Daniel Shmuglin <daniels@wix.com>
2012-08-30 19:59:43 +0300

    settings section - angularjs integration

--- googleadsebse-webapp/src/main/webapp/static/from-tom/scripts/script.js
+++ googleadsebse-webapp/src/main/webapp/static/from-tom/scripts/script.js
@@ -7,34 +7,39 @@ WixAdSenseSettings = (function () {
     drag = $('#dragMe');
 
     adSizes = {
-        'Horizontal':[
-            {name:'Leaderboard', width:728, height:90},
-            {name:'Banner', width:468, height:60},
-            {name:'Mobile Banner', width:320, height:50},
-            {name:'Half Banner', width:234, height:60}
-        ],
-        'Vertical'  :[
-            {name:'Wide Skyscraper', width:160, height:600},
-            {name:'Skyscraper', width:120, height:600},
-            {name:'Vertical Banner', width:160, height:240}
-        ],
-        'Square'    :[
-            {name:'Square', width:250, height:250},
-            {name:'Small Square', width:200, height:200},
-            {name:'Large Rectangle', width:336, height:280},
-            {name:'Medium Rectangle', width:300, height:250},
-            {name:'Small Rectangle', width:180, height:150},
-            {name:'Button', width:125, height:125, default:true}
-        ],
-        'Link'      :[
-            {name:'Horizontal Large', width:728, height:15},
-            {name:'Horizontal Medium', width:468, height:15},
-            {name:'Vertical X-Large', width:200, height:90},
-            {name:'Vertical Large', width:180, height:90},
-            {name:'Vertical Large', width:160, height:90},
-            {name:'Vertical Large', width:120, height:90}
-        ]
-
+        'Text':{
+            'Horizontal':[
+                {name:'Leaderboard', width:728, height:90},
+                {name:'Banner', width:468, height:60},
+                {name:'Mobile Banner', width:320, height:50},
+                {name:'Half Banner', width:234, height:60}
+            ],
+            'Vertical':[
+                {name:'Wide Skyscraper', width:160, height:600},
+                {name:'Skyscraper', width:120, height:600},
+                {name:'Vertical Banner', width:160, height:240}
+            ],
+            'Square':[
+                {name:'Square', width:250, height:250},
+                {name:'Small Square', width:200, height:200},
+                {name:'Large Rectangle', width:336, height:280},
+                {name:'Medium Rectangle', width:300, height:250},
+                {name:'Small Rectangle', width:180, height:150},
+                {name:'Button', width:125, height:125, default:true}
+            ]
+        },
+        'Link':{
+            'Horizontal':[
+                {name:'Horizontal Large', width:728, height:15},
+                {name:'Horizontal Medium', width:468, height:15}
+            ],
+            'Vertical':[
+                {name:'Vertical X-Large', width:200, height:90},
+                {name:'Vertical Large', width:180, height:90},
+                {name:'Vertical Large', width:160, height:90},
+                {name:'Vertical Large', width:120, height:90}
+            ]
+        }
     };
 
     populateSelect = function () {
--- googleadsebse-webapp/src/main/webapp/static/js/settings.js
+++ googleadsebse-webapp/src/main/webapp/static/js/settings.js
@@ -10,41 +10,64 @@ function SettingsCtrl($scope, $http, $resource){
 
     $scope.availableTypes = window.availableTypes;
 
-    $scope.adSizes = {
-        'Text':{
-            'Horizontal':[
-                {name:'Leaderboard', width:728, height:90},
-                {name:'Banner', width:468, height:60},
-                {name:'Mobile Banner', width:320, height:50},
-                {name:'Half Banner', width:234, height:60}
-            ],
-            'Vertical':[
-                {name:'Wide Skyscraper', width:160, height:600},
-                {name:'Skyscraper', width:120, height:600},
-                {name:'Vertical Banner', width:160, height:240}
-            ],
-            'Square':[
-                {name:'Square', width:250, height:250},
-                {name:'Small Square', width:200, height:200},
-                {name:'Large Rectangle', width:336, height:280},
-                {name:'Medium Rectangle', width:300, height:250},
-                {name:'Small Rectangle', width:180, height:150},
-                {name:'Button', width:125, height:125, default:true}
-            ]
+    $scope.adUnitTypes = [
+        {"id": "text", "label": "Text Only (default)"},
+        {"id": "text_image", "label": "Text and Image"},
+        {"id": "image", "label": "Image Only"}
+    ];
+
+    $scope.adSizes = [
+        {
+            'name':'Text',
+            'items':{
+                'Horizontal':[
+                    {name:'Leaderboard', width:728, height:90, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Banner', width:468, height:60, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Mobile Banner', width:320, height:50, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Half Banner', width:234, height:60, 'format':  "" + this.width + "x" + this.height + "_as"}
+                ],
+                'Square':[
+                    {name:'Square', width:250, height:250, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Small Square', width:200, height:200, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Large Rectangle', width:336, height:280, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Medium Rectangle', width:300, height:250, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Small Rectangle', width:180, height:150, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Button', width:125, height:125, default:true, 'format':  "" + this.width + "x" + this.height + "_as"}
+                ],
+                'Vertical':[
+                    {name:'Wide Skyscraper', width:160, height:600, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Skyscraper', width:120, height:600, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Vertical Banner', width:160, height:240, 'format':  "" + this.width + "x" + this.height + "_as"}
+                ]
+            }
         },
-        'Link':{
-            'Horizontal':[
-                {name:'Horizontal Large', width:728, height:15},
-                {name:'Horizontal Medium', width:468, height:15}
-            ],
-            'Vertical':[
-                {name:'Vertical X-Large', width:200, height:90},
-                {name:'Vertical Large', width:180, height:90},
-                {name:'Vertical Large', width:160, height:90},
-                {name:'Vertical Large', width:120, height:90}
-            ]
+        {
+            'name':'Link',
+            'items':{
+                'Horizontal':[
+                    {name:'Horizontal Large', width:728, height:15, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Horizontal Medium', width:468, height:15, 'format':  "" + this.width + "x" + this.height + "_as"}
+                ],
+                'Vertical':[
+                    {name:'Vertical X-Large', width:200, height:90, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Vertical Large', width:180, height:90, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Vertical Medium', width:160, height:90, 'format':  "" + this.width + "x" + this.height + "_as"},
+                    {name:'Vertical Small', width:120, height:90, 'format':  "" + this.width + "x" + this.height + "_as"}
+                ]
+            }
         }
+    ];
+
+    window.adTypeAngularJsHandler = function(type) {
+        var prevValue = undefined;
+        $scope.$apply(function() {
+            prevValue = $scope.settings.adType;
+            $scope.settings.adType = type;
+        });
+        return prevValue;
     };
+
+    $scope.adSizesFlattened = _.flatten(_.map($scope.adSizes, function(t) { return _.values(t.items) }));
 
     $scope.Settings = $resource(window.contentRoot + '/settings/:instanceId/:componentId', {componentId: '@widgetId.componentId', instanceId: '@widgetId.instanceId'});
 
@@ -54,19 +77,37 @@ function SettingsCtrl($scope, $http, $resource){
         $scope.settings.$save();
         Wix.closeSettings();
     }
+
+    $scope.picked = function(name) {
+        var item = _.find($scope.adSizesFlattened, function(o) { return o.name == name});
+        $scope.settings.width = item.width;
+        $scope.settings.height = item.height;
+        $scope.settings.format = item.format;
+    }
 }
 
 /* Settings UI scripts */
 
 $(document).ready(function() {
-    $("#ad-type-text").attr('checked', 'checked');
+
+    var prevValue = undefined;
+
     $("[name=ad-type]").change(function(){
         if ($('#ad-type-text').attr('checked') == 'checked'){
             $('#ad-type-list').removeAttr('disabled');
+            $(".ad-type-link-sizes").hide();
+            $(".ad-type-text-sizes").show();
+            window.adTypeAngularJsHandler(prevValue ? prevValue : "text");
         }else{
             $('#ad-type-list').attr('disabled', 'disabled');
+            $(".ad-type-link-sizes").show();
+            $(".ad-type-text-sizes").hide();
+            prevValue = window.adTypeAngularJsHandler("link_unit");
         }
     });
+
+    $("#ad-type-text").attr('checked', 'checked');
+    $(".ad-type-link-sizes").hide();
 
     $('.settings-ad-size').tooltip({
         selector: "a[rel=tooltip]"
